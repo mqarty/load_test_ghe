3f4e97830361e884ec6d3d8129a7949e8f5ff851
Marty Berryman <mqarty@gmail.com>
2018-10-19 16:16:32 -0700

    Initial commit for load testing ghe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # load_test_ghe
+Populate GHE with repos
+
+# Run
+1. docker-compose up -d
+2. docker-compose run load_test
---
+++ load_test/load_test/functions/run_it.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import base64
+import requests
+import grequests
+import logging
+import logging.handlers
+import os
+import requests
+import sys
+
+from subprocess import call
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+USERS_REPOS_URI = "/api/v3/user/repos" # /rest/api/2/issue/{issue_key}?expand=changelog"
+
+
+def _create_git_directory(directory, repo, filename):
+    logger.info("_create_git_directory :: {}/{}/{}".format(directory, repo, filename))
+    # os.chdir(directory)
+
+    # call ('git init {}'.format(repo), shell=True)
+    # call ('cd {}'.format(repo), shell=True)
+    # logger.info("Directory :: {}".format(directory))
+
+    # os.chdir(directory+str(repo))
+    # call ('pwd', shell=True)
+    # call('echo "{} try" > {}'.format(repo, filename), shell=True)
+    # call('git add {}'.format(filename), shell=True)
+
+def _git_commit_push(repo):
+    logger.info("_git_commit_push :: {}".format(repo))
+    # call('git commit -m "Try {}/{} times"'.format(repo, repo), shell=True)
+    # call('git remote add origin git@ghe-dev.sphereci.com:george/{}.git'.format(repo), shell=True)
+    # call('git push -u origin master', shell=True)
+
+def main():
+    num = 2
+    directory = os.getcwd()+"/"
+    filename = 'try-num-times.txt'.format(num)
+
+    url = "https://ghe-dev.sphereci.com/{}".format(USERS_REPOS_URI)
+    header = {"Authorization": "token {}".format(os.environ['TOKEN'])}
+
+    rs = (
+        grequests.post(
+            url, 
+            headers=header, 
+            payload={'name': str(repo)}, 
+            hooks={'pre_request':_create_git_directory(directory, repo, filename), 'post_request': _git_commit_push(repo)}
+            ) for repo in xrange(0, num)
+        )
+    
+    results = grequests.map(rs)
+
+    logger.info(results)
+  
+if __name__== "__main__":
+    main()
