d8dc905087dab273195240736589ae4f9ff00859
Marty Berryman <mqarty@gmail.com>
2018-10-24 12:00:46 -0700

    Fixes to many things that weren't working in Docker or github api

--- load_test/load_test/functions/run_it.py
+++ load_test/load_test/functions/run_it.py
@@ -3,6 +3,7 @@
 
 import base64
 import grequests
+import json
 import logging
 import logging.handlers
 import os
@@ -10,7 +11,6 @@ import requests
 import shutil
 import sys
 
-from github import Github
 from subprocess import call
 from tempfile import mkdtemp
 
@@ -20,7 +20,7 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
-USERS_REPOS_URI = "/api/v3/user/repos"
+USERS_REPOS_URI = "/user/repos"
 
 
 def hook_factory(*factory_args, **factory_kwargs):
@@ -40,38 +40,71 @@ def _git_steps(repo):
     try:
         # Fix this section
         repo_dir = mkdtemp()
-        r = git.Repo.init(repo_dir)
+        os.chdir(repo_dir)
+
+        call ('git init', shell=True)
+
+        logger.debug("Repo directory :: {} ".format(os.getcwd()))
+
+        file_path = '{}/{}'.format(repo_dir, filename)
+
+        with open(file_path, "w") as tmp:
+            tmp.write('{} try {}'.format(repo, filename))
+
+        call('git add {}'.format(file_path), shell=True)
+
         commit_message = "Try {}/{} times".format(repo, repo)
-        logger.info("Calling GIT commands in {}".format(repo_dir))
-        
-        with open(path, "w") as tmp:
-            tmp.write('{} try" > {}'.format(repo, filename))
-            r.index.add([tmp])
-            r.index.commit(commit_message)
+        call('git -c user.name="test" -c user.email="test@email.org" commit -m "{}"'.format(commit_message), shell=True)
+
+        https_git_uri = '{protocol}://{username}:{token}@{hostname}/{user_or_org}/{repo_name}.git'.format(**{
+            'protocol': os.environ['GITHUB_PROTOCOL'],
+            'username': os.environ['GITHUB_USERNAME'],
+            'token': os.environ['GITHUB_TOKEN'],
+            'hostname': os.environ['GITHUB_HOSTNAME'],
+            'user_or_org': os.environ['GITHUB_USER_OR_ORG_NAME'],
+            'repo_name': repo
+        })
+        logger.debug("Github URI :: {}".format(https_git_uri))
+        call('git remote add origin {https_git_uri}'.format(**{
+            'https_git_uri': https_git_uri
+            }), shell=True)
+        call('git push -u origin master', shell=True)
+    except Exception as e:
+        logger.error(e)
+        raise e
     finally:
+        logger.info("Cleaning up :: {} ".format(repo_dir))
         os.chdir(old_wd)
         shutil.rmtree(repo_dir)
 
 def main():
     logger.info("being main()")
         
-    URI = "{}://{}{}".format(os.environ['GITHUB_PROTOCOL'], os.environ['GITHUB_HOSTNAME'], USERS_REPOS_URI)
+    URI = "{}://api.{}{}".format(os.environ['GITHUB_PROTOCOL'], os.environ['GITHUB_HOSTNAME'], USERS_REPOS_URI)
     logger.info("Using URI :: {}".format(URI))
 
-    header = {"Authorization": "token {}".format(os.environ['GITHUB_TOKEN'])}
+    header = {
+        "Authorization": "token {}".format(os.environ['GITHUB_TOKEN']),
+        "Accept": "application/vnd.github.v3.full+json"
+    }
 
-    rs = (
+    rs = [
         grequests.post(
             URI,
             headers=header,
-            data={'name': str(repo)},
-            hooks={'response': [hook_factory(repo=repo)]},            
+            data=json.dumps({
+                'name': str(repo),
+                'description': "This is the {} repo".format(repo),
+                'private': False
+            }),
+            hooks={'response': [hook_factory(repo=repo)]},
             ) for repo in xrange(0, int(os.environ['NUMBER_OF_REPOS']))
-        )
-    
+    ]
+
     results = grequests.map(rs)
 
-    logger.info(results)
+    for result in results:
+        logger.info(result.json())
   
 if __name__== "__main__":
     main()
